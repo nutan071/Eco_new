1a86b6bd2950f3649d7b3e6203fe83fd85bbe180
Nutan <shekhadanutan123@gmail.com>
2024-07-27 09:44:55 +0530

    admin_testing

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Admin\Products;  
+use App\Models\Admin\Products;
 
 class AdminController extends Controller
 {
@@ -14,3 +14,4 @@ class AdminController extends Controller
     }
 
 }
+
